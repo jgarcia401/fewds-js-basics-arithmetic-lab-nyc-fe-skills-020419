1d2c222ad356cc0d01ed5f7c7e0202a2668c8c13
resourceful-transpiler-3397 <jgarcia401@gmail.com>
2019-03-07 01:51:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,5 +10,6 @@ You should NOT try to redefine any of the above variables.
 
 const newID = (1000000000 + oldID);
 
-const agelsValid = true .currentAge
+const agelsValid = Number.isInteger(currentAge)
+
 
